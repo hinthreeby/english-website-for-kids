bbb8418fc0f7592b714dc1e37ecb798b4400c201
hinthreeby <24520717@gm.uit.edu.vn>
2026-05-13 19:11:21 +0700

    fix vite api env

--- client/src/lib/api.js
+++ client/src/lib/api.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const API_BASE_URL = process.env.VITE_API_URL || 'http://localhost:5000';
+const API_BASE_URL =
+  import.meta.env.VITE_API_URL ||
+  "https://english-website-for-kids.onrender.com";
 
 const api = axios.create({
   baseURL: API_BASE_URL,
